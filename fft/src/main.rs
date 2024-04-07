c34e6a6170222a8e383ddaa65e88714a2f45cb39
Jacob Malloy <jmalloy1@vols.utk.edu>
2024-04-07 15:54:24 -0400

    work on making the code more effecient by removing the recursion

--- fft/src/main.rs
+++ fft/src/main.rs
@@ -1,39 +1,36 @@
 use std::{f64::consts::PI, hint::black_box, io::BufRead};
 use imaginary::imaginary::Imag;
 
-
+#[allow(dead_code)]
 fn dft<'a,T>(input:&'a[T])->Vec<Imag>
 where T:std::ops::Mul<Imag,Output = Imag> + Copy{
-    let N = input.len();
-    (0..N).map(|k|input.iter().copied().enumerate().map(|(n,x)|x*Imag::euler(-2.0*(n as f64)*PI*(k as f64)/(N as f64))).sum::<Imag>()).collect()
+    let len = input.len();
+    (0..len).map(|k|input.iter().copied().enumerate().map(|(n,x)|x*Imag::euler(-2.0*(n as f64)*PI*(k as f64)/(len as f64))).sum::<Imag>()).collect()
 }
 
 fn idft<'a,T>(input:&'a[T])->Vec<Imag>
 where T:std::ops::Mul<Imag,Output = Imag> + Copy{
-    let N = input.len();
-    (0..N).map(|k|(1.0/(N as f64))*input.iter().copied().enumerate().map(|(n,x)|x*Imag::euler(2.0*(n as f64)*PI*(k as f64)/(N as f64))).sum::<Imag>()).collect()
+    let len = input.len();
+    (0..len).map(|k|(1.0/(len as f64))*input.iter().copied().enumerate().map(|(n,x)|x*Imag::euler(2.0*(n as f64)*PI*(k as f64)/(len as f64))).sum::<Imag>()).collect()
 }
 
+#[inline(never)]
+fn split_even_and_odd(input:&mut [Imag]){
+    let n = input.len();
 
-fn fft_internal(input:&mut [Imag],factor:&[Imag]){
-    assert!(input.len().count_ones()==1);
-    let N = input.len();
-    if N <= 1{
-        return;
-    } 
     let even:Vec<Imag> = input.iter().step_by(2).map(|x|*x).collect::<Vec<Imag>>();
     let odd:Vec<Imag> = input.iter().skip(1).step_by(2).map(|x|*x).collect::<Vec<Imag>>();
 
-    input[0..N/2].copy_from_slice(&even);
-    input[N/2..N].copy_from_slice(&odd);
-    drop(even);
-    drop(odd);
+    input[0..n/2].copy_from_slice(&even);
+    input[n/2..n].copy_from_slice(&odd);
 
-    fft_internal(&mut input[0..N/2],factor);
-    fft_internal(&mut input[N/2..N],factor);
+}
 
-    let factor_mul = factor.len()/N;
-    let s = input.split_at_mut(N/2);
+#[inline(never)]
+fn combine_step(input:&mut [Imag],factor:&[Imag]){
+    let n = input.len();
+    let factor_mul = factor.len()/(n/2);
+    let s = input.split_at_mut(n/2);
     let even_iter = s.0.iter_mut(); 
     let odd_iter = s.1.iter_mut();
     let factor_iter = factor.iter().step_by(factor_mul);
@@ -41,41 +38,40 @@ fn fft_internal(input:&mut [Imag],factor:&[Imag]){
         let tmp = *odd * *factor;
         (*even,*odd) = (*even+tmp ,*even-tmp );
     }
-    
+}
+
+
+fn fft_internal(input:&mut [Imag],factor:&[Imag]){
+    assert!(input.len().count_ones()==1);
+    let n = input.len();
+
+    let mut len = n;
+    while len > 1{
+        for i in input.chunks_exact_mut(len){
+            split_even_and_odd(i);
+        }
+        len /= 2;
+    }
+
+    let mut len = 2;
+    while len <= n{
+        for i in input.chunks_exact_mut(len){
+            combine_step(i, factor)
+        }
+        len *= 2;
+    }  
 }
 
 
 
 fn fft<T>(input:&[T])->Vec<Imag>
 where Imag:From<T>,T:Copy{
-    let N = input.len();
+    let n = input.len();
     let mut return_vec:Vec<Imag> = input.iter().copied().map(|x|Imag::from(x)).collect();
-    let factor:Vec<Imag> = (0..N).map(|i|Imag::euler(-2.0*PI*((i ) as f64)/(N as f64))).collect();
+    let factor:Vec<Imag> = (0..n/2).map(|i|Imag::euler(-2.0*PI*((i ) as f64)/(n as f64))).collect();
     fft_internal(&mut return_vec,&factor);
     return return_vec;
 }
-
-/* 
-fn fft<'a,T>(input:&'a[T])->Vec<Imag>
-where T:std::ops::Mul<Imag,Output = Imag>, T:Copy
-{
-    assert!(input.len().count_ones()==1);
-    let N = input.len();
-    if input.len() <= 8{ 
-        return dft(input);
-    }
-    
-    let even:Vec<Imag> = fft(&input.iter().step_by(2).map(|x|*x).collect::<Vec<T>>());
-    let odd:Vec<Imag> = fft(&input.iter().skip(1).step_by(2).map(|x|*x).collect::<Vec<T>>());
-    let vals = (0..N/2).map(|n|Imag::euler(-2.0*PI*(n as f64)/(N as f64)));
-    let vals2 = (N/2..N).map(|n|Imag::euler(-2.0*PI*(n as f64)/(N as f64)));
-    let mut return_val:Vec<Imag> = vals.zip(even.iter().zip(odd.iter())).map(|(factor,(even,odd))|*even + (&factor*odd)).collect();
-    return_val.extend(vals2.zip(even.iter().zip(odd.iter())).map(|(factor,(even,odd))|*even + (&factor*odd)));
-    return return_val
-}
-
-*/
-
 
 fn main() {
     let file = std::fs::File::open("../tmp2.data").unwrap();
@@ -83,14 +79,14 @@ fn main() {
     let data:Vec<i64> = bufreader.lines().map(|x|x.unwrap().parse::<i64>().unwrap()).collect();
     
     let time = std::time::Instant::now(); 
-    let result = black_box(fft(&data));
+    let _result = black_box(fft(&data));
     println!("{}",(time.elapsed().as_micros() as f64)/1000000.0);
     
     
     let tmp = [-9.0,4.0,-5.0,7.0,-2.0,3.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
     let tmp2 = [1.0,3.0,-5.0,2.0,6.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
-    let A = fft(&tmp);
-    let B = fft(&tmp2);
+    let a = fft(&tmp);
+    let b = fft(&tmp2);
     /*for i in A.iter(){
        println!("{}",i); 
     }
@@ -100,7 +96,7 @@ fn main() {
     }
 
     println!("");*/
-    let tmp3:Vec<Imag> = A.iter().zip(B.iter()).map(|(a,b)|a * b).collect();
+    let tmp3:Vec<Imag> = a.iter().zip(b.iter()).map(|(a,b)|a * b).collect();
     /*for i in tmp3.iter(){
         println!("{}",i)
     }
